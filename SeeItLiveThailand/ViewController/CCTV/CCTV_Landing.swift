e71f269847e03d9c596ac03de535b48c37de2862
teerstudio <ddz_ttt@hotmail.com>
2016-10-03 16:05:36 +0700

    Tableview ทับ BannerView

--- SeeItLiveThailand/ViewController/CCTV/CCTV_Landing.swift
+++ SeeItLiveThailand/ViewController/CCTV/CCTV_Landing.swift
@@ -7,21 +7,34 @@
 //
 
 import UIKit
+import SnapKit
 
 class CCTV_Landing: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet weak var view_TopSlider: UIView!
-    @IBOutlet weak var tb_Lists: UITableView!
+    @IBOutlet var view_TopSlider: UIView!
+    @IBOutlet var tb_Lists: UITableView!
+    
+    lazy var box = UIView() // SnapKit view
+
+    
+    var vWidth:CGFloat = 0
+    var vHeight:CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("CCTV_Landing Loaded")
+        vWidth = self.view.frame.width
+        vHeight = self.view.frame.height
+        
+        
         
 //        self.title = "CCTV"
         
         //tb_Lists.contentOffset.y = view_TopSlider.frame.height
         
-
+        //tb_Lists.backgroundColor = UIColor.clear
+        
+        design()
         // Do any additional setup after loading the view.
     }
 
@@ -41,8 +54,32 @@ class CCTV_Landing: UIViewController, UITableViewDelegate, UITableViewDataSource
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        //let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
         cell.textLabel?.text = "row : \(indexPath.row)"
         return cell
+    }
+    
+    // MARK: - Design Zone
+    func design() {
+        
+//        self.view_TopSlider.snp.makeConstraints { (make) -> Void in
+//            make.width.equalTo(200)
+//            make.height.equalTo(80)
+//            //make.center.equalTo(self.view)
+//            make.top.equalTo(0)
+//            make.left.equalTo(0)
+//        }
+//        
+//        self.tb_Lists.snp.makeConstraints { (make) -> Void in
+//            make.width.equalTo(self.vWidth)
+//            make.height.equalTo(self.vHeight - self.view_TopSlider.frame.height)
+//            //make.center.equalTo(self.view)
+//            make.top.equalTo(self.view_TopSlider.frame.height)
+//            make.left.equalTo(0)
+//        }
+        
+//        self.tb_Lists.contentInset.top = CGFloat(100) // self.view_TopSlider.frame.height
+        
     }
     
 
